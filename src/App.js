ecce85b262710211346c63553cafa48e5afd487a
Mahsa_MCSD <mahsaashrafi71@outlook.com>
2020-08-22 18:06:02 +0430

    comonentsstructure  is completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello Mahsa</div>;
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
   }
 }
 
