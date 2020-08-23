88a757a2b00aa6a87ac354105baeefddfaf8f886
Mahsa_MCSD <mahsaashrafi71@outlook.com>
2020-08-23 22:14:11 +0430

    navigate to the page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+   
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
       </div>
     );
   }
 }
+
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+
+    </div>
+  );
+};
+
+
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./menu-item.style.scss";
-const MenuItem = ({ title, imageUrl, id, size }) => (
-  <div className={`${size} menu-item`}>
+import {withRouter} from 'react-router-dom'
+
+const MenuItem = ({ title, imageUrl, size ,history,linkUrl,match}) => (
+  <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
     <div
       className="background-image"
       style={{
@@ -15,4 +17,4 @@ const MenuItem = ({ title, imageUrl, id, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
